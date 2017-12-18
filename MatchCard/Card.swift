156fc9f203003bae33ab93e0a6407a1a478974c2
anuj babu <25268716+ShwethaVasudevan@users.noreply.github.com>
2017-12-18 23:30:14 +0530

    First commit: UI scroll view

---
+++ MatchCard/Card.swift
@@ -0,0 +1,21 @@
+//
+//  Card.swift
+//  MatchCard
+//
+//  Created by anuj babu on 18/12/17.
+//  Copyright © 2017 TagTechLtd. All rights reserved.
+//
+
+import UIKit
+
+class Card: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MatchCard/CardModel.swift
@@ -0,0 +1,13 @@
+//
+//  CardModel.swift
+//  MatchCard
+//
+//  Created by anuj babu on 18/12/17.
+//  Copyright © 2017 TagTechLtd. All rights reserved.
+//
+
+import UIKit
+
+class CardModel: NSObject {
+
+}
